ae4db5847f1bf339204ba00717ee7cb626a59e39
Judah Sullivan <judahsullivan.dev@gmail.com>
2023-06-12 09:08:46 -0500

    fixed syntax errors

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,26 +5,26 @@ import Hero from '../../public/assets/hero.png'
 export default function HomePage() {
   return(
     <div className="grid-halves h-screen-navbar">
-    <div class="bg-teal border-right">
-    <div class="column-padding">
-    <div class="tablet-center">
-    <div class="content-grid home-hero">
+    <div className="bg-teal border-right">
+    <div className="column-padding">
+    <div className="tablet-center">
+    <div className="content-grid home-hero">
     <h1>
     The Most <br /> epic products
     </h1>
-    <p class="section-subtitle">
+    <p className="section-subtitle">
     All the Most epic products you will find on the internet all in one place!
     </p>
-    <Link class="large-button" href="/products" >
-    <div class="large-button-text">Explore Our Products</div>
+    <Link className="large-button" href="/products" >
+    <div className="large-button-text">Explore Our Products</div>
     </Link>
     </div>
     </div>
     </div>
     </div>
-    <div class="bg-salmon">
-    <div class="column-padding centered">
-    <div class="callout-wrap">
+    <div classNames="bg-salmon">
+    <div className="column-padding centered">
+    <div className="callout-wrap">
     <Image src={Hero} alt="Hero Image" className="callout-image"/>
     </div>
     </div>
